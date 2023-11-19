d34f1225709b7101390d488c2c5e613ad113936b
Jigalee Puff <marsayah@gmail.com>
2023-11-19 13:26:36 +0000

    final

--- src/main/java/com/cbfacademy/apiassessment/App.java
+++ src/main/java/com/cbfacademy/apiassessment/App.java
@@ -2,23 +2,14 @@ package com.cbfacademy.apiassessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 
 @SpringBootApplication
 
-=======
-
-
-@SpringBootApplication
->>>>>>> origin/main
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-<<<<<<< HEAD
 	
-=======
->>>>>>> origin/main
 }
 	
